2525da4cdced9b5cd8ed49510c093ccfe4eaa4e2
roryg11 <Rory.C.Grant@gmail.com>
2014-11-10 14:38:25 -0800

    todo app assessment

--- lib/todo_app.rb
+++ lib/todo_app.rb
@@ -3,4 +3,51 @@ class TodoApp < CommandLineApp
     @input = input
     @output = output
   end
+
+  def run
+    puts "Welcome"
+    puts "'list' to list projects"
+    puts "'create' to create a new project"
+    puts "'edit' to edit a project"
+    puts "Please enter the new project name:\n "
+
+    input = gets.chomp
+
+    projects = ""
+
+    while input != 'quit'
+
+      if input == 'list'
+        if projects== ""
+          puts "Projects:\n  none"
+        else
+          puts "Projects:\n  #{projects}"
+        end
+
+      elsif input == 'create'
+        puts "Please enter the new project name:\n"
+        projects = gets.chomp
+        puts "Projects:\n  #{projects}"
+
+      elsif input == 'edit'
+        puts "Please enter the project name to edit:\n"
+        projects = gets.chomp
+        puts "Please enter the new project name:\n"
+        projects = gets.chomp
+        puts "Projects:\n  #{projects}"
+
+      elsif input == 'delete'
+        puts "Please enter the project name to delete:\n"
+        input = gets.chomp
+        projects = projects.gsub!("#{input}", "")
+
+      end
+      input = gets.chomp
+    end
+
+  end
+
+  def real_puts message = ""
+    $stdout.puts message
+  end
 end
